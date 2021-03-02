cb3de282bd619aec928dff5ea598f1244e306eb5
Marcos Beirigo <marcosbeirigo@gmail.com>
2021-03-02 08:51:03 -0500

    A Humble commit

---
+++ lib/marcos.rb
@@ -0,0 +1,10 @@
+require "marcos/version"
+
+module Marcos
+  class Error < StandardError; end
+  # Your code goes here...
+
+
+end
+
+puts "hello, I'm Marcos!"
---
+++ lib/marcos/version.rb
@@ -0,0 +1,3 @@
+module Marcos
+  VERSION = "0.1.0"
+end
